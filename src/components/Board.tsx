12ecedabeb615fdd1eb6c363fbc0f219f9c53016
Çağatay Emre Bilgen <cebilgen@gmail.com>
2019-01-02 17:12:00 +0000

    Add board background

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -10,11 +10,16 @@ interface BoardProps {
 export default function Board(props: BoardProps): JSX.Element {
     return (
         <div class="board" tabIndex={0}>
-            {R.times((n) => 
-                <Square     
-                    value={props.values[n]}
-                />, 16
-            )}
+            <div class="board__background">
+                {R.repeat(<div class="board__square board__square--bg" />, 16)}
+            </div>
+            <div class="board__actual">
+                {R.times((n) => 
+                    <Square     
+                        value={props.values[n]}
+                    />, 16
+                )}
+            </div>
         </div>
     )
 }
