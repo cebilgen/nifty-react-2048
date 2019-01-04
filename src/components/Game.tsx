2429a673c5cd035057acf1a45798cc8e1e018ebe
Çağatay Emre Bilgen <cebilgen@gmail.com>
2019-01-04 13:23:00 +0000

    Add first square of a new game

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -2,6 +2,7 @@ import {h, Component} from 'preact'
 import * as R from 'ramda'
 
 import Board from './Board'
+import { BoardOps } from '../logic'
 
 
 interface GameState {
@@ -18,7 +19,7 @@ export default class Game extends Component<any, GameState> {
 
     newGame() {
         this.setState({
-            board: R.repeat(0, 16),
+            board: BoardOps.newSquare(R.repeat(0, 16)),
             score: 0,
             noChange: R.repeat(false, 4)
         })
