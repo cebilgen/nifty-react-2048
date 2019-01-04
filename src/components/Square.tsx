cde5984f18d983862dee4629c0de0d8d2bb6dcb8
Çağatay Emre Bilgen <cebilgen@gmail.com>
2019-01-04 14:01:00 +0000

    Add square colors
    Color palette taken from duckduckgo.com.
    Searching "2048" brings up an easter egg 2048 implementation.

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -6,8 +6,11 @@ interface SquareProps {
 }
 
 export default function Square(props: SquareProps): JSX.Element {
+    const selfClass: string[] = ['board__square']
+    selfClass.push('board__square--val-' + props.value.toString())
+
     return (
-        <div class="board__square">
+        <div class={selfClass.join(' ')}>
             {props.value}
         </div>
     )
