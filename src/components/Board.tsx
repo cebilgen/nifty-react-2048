75432fc6d8f2bb33548f7d0cf52df051d46698dc
Çağatay Emre Bilgen <cebilgen@gmail.com>
2019-01-04 13:18:00 +0000

    Implement keyboard controls

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -4,12 +4,13 @@ import Square from './Square';
 
 
 interface BoardProps {
+    onKeyDown: (ev: KeyboardEvent) => void
     values: number[] 
 }
 
 export default function Board(props: BoardProps): JSX.Element {
-    return (
-        <div class="board" tabIndex={0}>
+    return (        
+        <div class="board" tabIndex={0} onKeyDown={(ev: KeyboardEvent) => props.onKeyDown(ev)}>
             <div class="board__background">
                 {R.repeat(<div class="board__square board__square--bg" />, 16)}
             </div>
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -24,10 +24,29 @@ export default class Game extends Component<any, GameState> {
         })
     } 
 
+    onKeyDown(ev: KeyboardEvent) {
+        if (ev.key === 'ArrowUp') {
+            // slide up 
+        }
+        if (ev.key === 'ArrowRight') {
+            // slide right 
+        }
+        else if (ev.key === 'ArrowDown') {
+            // slide down
+        } 
+        else if (ev.key === 'ArrowLeft') {
+            // slide left 
+        } 
+        else if (ev.key === 'Backspace') {
+            // undo?
+        }
+    }
+
     render(): JSX.Element {
         return (
             <div class="game">
                 <Board
+                    onKeyDown={(ev: KeyboardEvent) => this.onKeyDown(ev)}
                     values={this.state.board}
                 />
             </div>
